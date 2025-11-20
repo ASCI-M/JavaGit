a59085b81b714bfd3a7987757a7f7df3eeaace17
Modit Sanyasi <moditsanyasi1234@gmail.com>
2025-11-20 10:33:17 +0545

    This is my first push

---
+++ CHIYAPASAL.java
@@ -0,0 +1,37 @@
+public class CHIYAPASAL{
+
+	public static void main(String[] args){
+		System.out.println("------------------");
+		System.out.println("------------------");
+		System.out.println("Welcome to our shop : The Chiya Pasal...!!!...");
+		System.out.println("Hamro Chiya Pasal ma hajur haru ko swagatxa...!!!...");
+		System.out.println("------------------");
+		System.out.println("------------------");	
+		System.out.println("......MENU.......");
+		System.out.println("------------------");
+		System.out.println("...1.MO:MO...");
+		System.out.println("...2.Thuppa...");
+		System.out.println("...3.Chowmein...");
+		System.out.println("------------------");
+		System.out.println("------------------");
+		System.out.println("..OUR SPECIAL ITEMS...");
+		System.out.println("------------------");
+		System.out.println("...1.Chiya(tea)...");
+		System.out.println("...2.Samosa...");
+		System.out.println("...3.Pakoda...");
+		System.out.println("------------------");
+		System.out.println("------------------");
+		System.out.println("...Opening Hours...");
+		System.out.println("------------------");
+		System.out.println("...Open from : 9:00 AM to 5:00 PM...");
+		System.out.println("...Opens all days of the week...");
+		System.out.println("------------------");
+		System.out.println("-------------------");
+		System.out.println("...Thankyou..!!!...Please Visit Again...!!!...");
+		System.out.println("------------------");
+		System.out.println("------------------");
+		
+		}
+
+
+}
